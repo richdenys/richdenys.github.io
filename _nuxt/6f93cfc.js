72d62dbe6c81493be4b85ef8f8239a764c0a67df
unknown <richdenys_dc@hotmail.com>
2020-12-12 13:51:17 -0500

    version 1.4

---
+++ _nuxt/6f93cfc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{439:function(n,t,e){"use strict";e.r(t);var r=e(53),l=e(64),c=e.n(l),o=e(374),component=Object(r.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("v-container",{attrs:{fluid:"","grid-list-md":""}})}),[],!1,null,null,null);t.default=component.exports;c()(component,{VContainer:o.a})}}]);
